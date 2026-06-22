3316c087e38bd594a8edf4f6175d1ade82775984
Hodaahmed18 <royalpain687@gmail.com>
2026-06-22 09:40:49 +0000

    Added Python TDD exercises

--- functions.py
+++ functions.py
@@ -42,3 +42,82 @@ def is_palindrome(text):
         return True
     else:
         return False
+
+def find_longest_word(words):
+    longest = words[0]
+
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+
+    return longest
+
+
+def filter_odd_numbers(numbers):
+    result = []
+
+    for number in numbers:
+        if number % 2 != 0:
+            result.append(number)
+
+    return result
+
+
+def calculate_discount(price, discount):
+    return price - (price * discount / 100)
+
+
+def find_shortest_word(words):
+    shortest = words[0]
+
+    for word in words:
+        if len(word) < len(shortest):
+            shortest = word
+
+    return shortest
+
+
+def grade_score(score):
+    if score >= 70:
+        return "A"
+    elif score >= 60:
+        return "B"
+    elif score >= 50:
+        return "C"
+    else:
+        return "Fail"
+
+
+def mask_email(email):
+    username, domain = email.split("@")
+    return username[0] + "***@" + domain
+
+
+def count_passes_and_fails(scores):
+    passes = 0
+    fails = 0
+
+    for score in scores:
+        if score >= 40:
+            passes += 1
+        else:
+            fails += 1
+
+    return {
+        "passes": passes,
+        "fails": fails
+    }
+
+
+def count_words(sentence):
+    counts = {}
+
+    words = sentence.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
--- test_functions.py
+++ test_functions.py
@@ -19,3 +19,54 @@ def test_filter_even_numbers():
 
 def test_is_palindrome():
     assert is_palindrome("level") == True
+
+def test_find_longest_word():
+    assert find_longest_word(
+        ["cat", "elephant", "dog"]
+    ) == "elephant"
+
+
+def test_filter_odd_numbers():
+    assert filter_odd_numbers(
+        [1, 2, 3, 4, 5]
+    ) == [1, 3, 5]
+
+
+def test_calculate_discount():
+    assert calculate_discount(
+        100, 20
+    ) == 80
+
+
+def test_find_shortest_word():
+    assert find_shortest_word(
+        ["elephant", "cat", "giraffe"]
+    ) == "cat"
+
+
+def test_grade_score():
+    assert grade_score(75) == "A"
+
+
+def test_mask_email():
+    assert mask_email(
+        "john@gmail.com"
+    ) == "j***@gmail.com"
+
+
+def test_count_passes_and_fails():
+    assert count_passes_and_fails(
+        [20, 50, 80]
+    ) == {
+        "passes": 2,
+        "fails": 1
+    }
+
+
+def test_count_words():
+    assert count_words(
+        "cat cat dog"
+    ) == {
+        "cat": 2,
+        "dog": 1
+    }
